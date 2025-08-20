dbe9f071f686507dc813f8e6b91cebad5b691b54
George Stephanis <daljo628@gmail.com>
2025-08-20 14:54:37 -0400

    First pass at an iteration to let the theme modify itself to populate the nav_menu ids.

--- _playground/setup.php
+++ _playground/setup.php
@@ -24,13 +24,50 @@ include_once __DIR__ . '/../inc/starter-content.php';
 $starter_content = \GeorgeTheme\StarterContent\get_starter_content();
 $nav_menus       = array();
 
+/**
+ * Takes a path, and an array of replacements.
+ *
+ * Searches for all the keys, and swaps them over
+ * to the replacements, in the directory specified.
+ *
+ * @param string $path.        The path to the directory to scan.
+ * @param array  $replacements An associative array, with the keys as the search, and the values as the replacements.
+ *
+ * @return void
+ */
+function replace_in_path( $path, $replacements ) {
+    $files = scandir( $path );
+
+    if ( $files && is_array( $files ) ) {
+        foreach ( $files as $file ) {
+            $abspath = $path . DIRECTORY_SEPARATOR . $file;
+            if ( in_array( $file, array( '.', '..' ) ) ) {
+                // If it's a directory reference, skip.
+                continue;
+            } elseif ( is_dir( $abspath ) ) {
+                // Recurse down into the directory.
+                replace_in_path( $abspath, $replacements );
+            } elseif ( is_writable( $abspath ) ) {
+                $markup = file_get_contents( $abspath );
+                $updated = str_replace( array_keys( $replacements ), array_values( $replacements ), $markup );
+                if ( $markup !== $updated ) {
+                    file_put_contents( $abspath, $updated );
+                }
+            }
+        }
+    }
+}
+
 if ( ! empty( $starter_content['nav_menus'] ) && is_array( $starter_content['nav_menus'] ) ) {
     foreach ( $starter_content['nav_menus'] as $slug => $properties ) {
         $nav_menu = wp_get_nav_menu_object( "{$slug}-menu" );
         if ( $nav_menu ) {
-            $nav_menus[ $slug ] = $nav_menu->term_id;
+            $key = sprintf( '<!-- wp:navigation {"slug":"%s",', esc_attr( $slug ) );
+            $nav_menus[ $key ] = sprintf( '<!-- wp:navigation {"ref":%d,', intval( $nav_menu->term_id ) );
         }
     }
 
-    update_option( 'blogdescription', wp_json_encode( $nav_menus ) );
+    replace_in_path( __DIR__ . '/../parts', $nav_menus );
+    replace_in_path( __DIR__ . '/../templates', $nav_menus );
+    replace_in_path( __DIR__ . '/../patterns', $nav_menus );
 }
