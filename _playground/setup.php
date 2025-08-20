fadb38bf3c4871b1d8125eb8955b848a8058ecb1
George Stephanis <daljo628@gmail.com>
2025-08-20 14:18:18 -0400

    Add in an iterator to test nav menus and fetching their ids.

--- _playground/setup.php
+++ _playground/setup.php
@@ -14,3 +14,23 @@ namespace GeorgeTheme\Setup;
 global $wp_rewrite;
 $wp_rewrite->set_permalink_structure('/%postname%/');
 $wp_rewrite->flush_rules();
+
+/**
+ * Get the nav menu ids for each slug, and map them in.
+ */
+
+include_once __DIR__ . '/../inc/starter-content.php';
+
+$starter_content = \GeorgeTheme\StarterContent\get_starter_content();
+$nav_menus       = array();
+
+if ( ! empty( $starter_content['nav_menus'] ) && is_array( $starter_content['nav_menus'] ) ) {
+    foreach ( $starter_content['nav_menus'] as $slug => $properties ) {
+        $nav_menu = wp_get_nav_menu_object( $slug );
+        if ( $nav_menu ) {
+            $nav_menus[ $slug ] = $nav_menu->term_id;
+        }
+    }
+
+    update_option( 'blogdescription', wp_json_encode( $nav_menus ) );
+}
